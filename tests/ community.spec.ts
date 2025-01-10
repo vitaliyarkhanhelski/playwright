59fcc907ccae3769204056b7f3a8d86926cef3c1
Vitaliy Arkhanhelski <Vitaliy.Arkhanhelski@zilliant.com>
2025-01-10 17:19:55 +0100

    init commit

---
+++ tests/ community.spec.ts
@@ -0,0 +1,130 @@
+import { test, expect } from '@playwright/test';
+const { readFileSync } = require('fs');
+const {promisify} = require('util');
+const exec = promisify(require('child_process').exec);
+
+let testDataJson = 'tests/register-business.json';
+let orgName = 'experience';
+let siteName = 'Apex_Hours';
+let ansiRegex = /\u001b\[[0-9;]*m/g;
+
+
+async function wait(page: Page): Promise<void> {
+  await page.waitForTimeout(2000);
+}
+
+function log(text: string): void {
+  console.log(text);
+}
+
+test('getAccounts', async ({page }) => {
+  let sfVersion = await exec(`sf --version`);
+  log('sfOutput: ' + sfVersion.stdout);
+
+  let sfOutput = await exec(`sf data query --query "SELECT Id, Name FROM Account LIMIT 5" --target-org ${orgName} --json`);
+  let sfResult = parseJson(sfOutput);
+  let records = getRecords(sfResult);
+
+  records.forEach((record: any) => {
+    console.log('Account Name: ' + record.Name);
+  });
+});
+
+///////////////////////////////////////////////////
+
+test('log', async ({page }) => {
+  await wait(page);
+  log('Hello World');
+});
+
+///////////////////////////////////////////////////
+
+test('basic test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page).toHaveTitle(/Playwright/); //like
+});
+
+///////////////////////////////////////////////////
+
+test('test Experience Cloud', async ({ page }) => {
+
+  let testData = readFileSync(testDataJson);
+  let testDataObj = JSON.parse(testData);
+
+  let firstName = testDataObj.firstName;
+  let lastName = testDataObj.lastName;
+
+  console.log('firstName: ' + firstName);
+  console.log('lastName: ' + lastName);
+
+
+  let site = await exec(`sf data query --query "SELECT Id, Name, GuestUserId, GuestUser.Name FROM Site WHERE Name = '${siteName}'" --target-org ${orgName} --json`);
+  let siteResult = parseJson(site);
+  let siteId = getFirstRecord(siteResult).Id;
+  console.log('siteId: ' + siteId);
+
+  let siteDetail = await exec(`sf data query --query "SELECT Id, SecureUrl FROM SiteDetail WHERE DurableId = '${siteId}'" --target-org ${orgName} --json`);
+  let siteDetailResult = parseJson(siteDetail);
+  let secureUrl = getFirstRecord(siteDetailResult).SecureUrl;
+  console.log('secureUrl: ' + secureUrl);
+
+  await page.goto(secureUrl);
+  await page.getByRole('link', { name: 'Previous' }).click();
+  await wait(page);
+  await page.getByRole('link', { name: 'Previous' }).click();
+  // await page.waitForTimeout(1000);
+  await page.getByRole('link', { name: 'Previous' }).click();
+  // await page.waitForTimeout(1000);
+  await page.getByRole('link', { name: 'Previous' }).click();
+  // await page.waitForTimeout(1000);
+  await page.getByRole('link', { name: 'Previous' }).click();
+  // await page.waitForTimeout(1000);
+  await expect(page.getByRole('main')).toContainText('INTERESTED IN GOING TO SPACE?');
+
+  await page.getByRole('button', { name: 'Log in' }).click();
+  // await page.waitForTimeout(1000);
+  await page.getByPlaceholder('Username').click();
+  await page.getByPlaceholder('Username').fill(firstName);
+  // await page.waitForTimeout(1000);
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill(lastName);
+  // await page.waitForTimeout(1000);
+  await page.getByRole('button', { name: 'Log in' }).click();
+
+  let url = new URL(page.url());
+  const urlParams = new URLSearchParams(url.search);
+  console.log('urlParams: ' + urlParams);
+  expect(urlParams.has('apexHours'));
+  expect(urlParams.has('startURL')).toBeTruthy();
+
+  let startUrl = urlParams.get('startURL');
+  console.log('startUrl: ' + startUrl);
+  await page.getByRole('link', { name: 'Forgot your password?' }).click();
+  await page.getByPlaceholder('Username').click();
+  await page.getByPlaceholder('Username').fill('Hello World');
+  await page.getByPlaceholder('Username').press('Enter');
+  await expect(page.locator('#centerPanel')).toContainText('PASSWORD RESET');
+});
+
+/////////////////////////////////////////////////// methods
+
+test('navigate to docs', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.getByRole('link', { name: 'Docs' }).click();
+  await expect(page).toHaveURL(/.*docs/);
+});
+
+function parseJson(obj: object): object {
+  let result = obj.stdout;
+  let cleanedResult = result.replace(ansiRegex, '');
+
+  return JSON.parse(cleanedResult);
+}
+
+function getFirstRecord(obj: object): string {
+  return obj.result.records[0]
+}
+
+function getRecords(obj: object): string[] {
+  return obj.result.records
+}
---
+++ tests/create-new-account.spec.ts
@@ -0,0 +1,116 @@
+import { test, expect, Page } from '@playwright/test';
+const { readFileSync } = require('fs');
+const {promisify} = require('util');
+const exec = promisify(require('child_process').exec);
+
+let accountDataJson = 'tests/account-data.json';
+let orgName = 'experience';
+let ansiRegex = /\u001b\[[0-9;]*m/g;
+
+let accountName: string;
+let accountNunber: string;
+let phone: string;
+let type: string;
+let industry: string;
+
+
+
+test('createNewAccount', async ({page }) => {
+  test.slow();
+  
+  populateAccountDataFromFile();
+  await removeTestData();
+  let sfUrl = await getUrl();
+
+  await page.goto(sfUrl);
+  await page.getByRole('link', { name: 'Accounts' }).click();
+  await page.getByRole('button', { name: 'New' }).click();
+
+  await populateNewAccountData(page);
+
+  await page.getByRole('button', { name: 'Save', exact: true }).click();
+
+  await page.waitForURL('**/view');
+  await page.reload();
+
+  //asserts
+  await page.waitForURL('**/view');
+  await page.waitForSelector('records-highlights2');
+  await expect(page.locator('records-highlights2')).toContainText(accountName);
+
+  let accountUrl = new URL(page.url());
+  log('accountUrl: ' + accountUrl.pathname);
+  let path_split = accountUrl.pathname.split('/');
+
+  expect(path_split[3]).toBe('Account');
+  expect(path_split[5]).toBe('view');
+
+  let accountId = path_split[4];
+  log('accountId: ' + accountId);
+
+  await removeTestData();
+});
+
+
+async function populateNewAccountData(page: Page) {
+  await page.getByLabel('*Account Name').click();
+  await page.getByLabel('*Account Name').fill(accountName);
+
+  await page.getByLabel('Account Number').click();
+  await page.getByLabel('Account Number').fill(accountNunber);
+
+  await page.getByRole('textbox', { name: 'Phone' }).click();
+  await page.getByRole('textbox', { name: 'Phone' }).fill(phone);
+
+  await page.getByRole('combobox', { name: 'Type' }).click();
+  await page.getByTitle('Customer - Direct', { exact: true }).click();
+
+  await page.getByRole('combobox', { name: 'Industry' }).click();
+  await page.getByText(industry).click();
+}
+
+async function getUrl() {
+  let sfVersion = await exec(`sf org open -o ${orgName} --path /lightning/page/home --url-only --json`);
+  let sfResult = parseJson(sfVersion);
+  return sfResult.result.url;
+}
+
+function populateAccountDataFromFile() {
+  let accountData = readFileSync(accountDataJson);
+  let accountDataObj = JSON.parse(accountData);
+
+  accountName = accountDataObj.name;
+  accountNunber = accountDataObj.accountNumber;
+  phone = accountDataObj.phone;
+  type = accountDataObj.type;
+  industry = accountDataObj.industry;
+}
+
+function parseJson(obj: object): object {
+  let result = obj.stdout;
+  let cleanedResult = result.replace(ansiRegex, '');
+
+  return JSON.parse(cleanedResult);
+}
+
+async function wait(page: Page): Promise<void> {
+  await page.waitForTimeout(2000);
+}
+
+function log(text: string): void {
+  console.log(text);
+}
+
+async function removeTestData(): Promise<void> {
+  try {
+    let deleteResult = await exec(`sf data delete record -s Account -w "Name='${accountName}'" -o ${orgName}`);
+    log('deleteResult: ' + deleteResult.stdout);
+  } catch (error) {
+    log('No matching record found to delete');
+  }
+
+  let queryResult = await exec(`sf data query -q "SELECT Id FROM Account WHERE Name='${accountName}'" -o ${orgName} --json`);
+  let queryJson = parseJson(queryResult);
+  expect(queryJson.result.records.length).toBe(0);
+}
+
