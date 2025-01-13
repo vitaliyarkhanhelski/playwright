97b64697d894f875f5bc1d64a7b708efce68af6c
Vitaliy Arkhanhelski <Vitaliy.Arkhanhelski@zilliant.com>
2025-01-13 16:26:28 +0100

    added script

---
+++ scripts/run-create-new-account.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the project directory
+cd /Users/varkh/Development/playwright/secondProject
+
+# Run the Playwright test
+npx playwright test tests/create-new-account.spec.ts
